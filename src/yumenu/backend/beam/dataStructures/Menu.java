89d7c9d09ed4afbd4152f951b6c75e920e64056d
FrancescoGhinamo <44404163+FrancescoGhinamo@users.noreply.github.com>
2019-10-14 19:14:39 +0200

    getByType funciton

--- src/yumenu/backend/beam/dataStructures/Menu.java
+++ src/yumenu/backend/beam/dataStructures/Menu.java
@@ -2,12 +2,25 @@ package yumenu.backend.beam.dataStructures;
 
 import java.util.ArrayList;
 
+import yumenu.backend.beam.recipe.RType;
 import yumenu.backend.beam.recipe.Recipe;
 
 public class Menu extends ArrayList<Recipe> {
 
 	
 	private static final long serialVersionUID = -8133597377913764662L;
+	
+	
+	public ArrayList<Recipe> getByType(RType recipeType) {
+		ArrayList<Recipe> res = new ArrayList<Recipe>();
+		for(Recipe r: this) {
+			if(r.getRecipeType().equals(recipeType)) {
+				res.add(r);
+			}
+		}
+		
+		return res;
+	}
 	
 	public ArrayList<Recipe> getRecipes() {
 		return this;
