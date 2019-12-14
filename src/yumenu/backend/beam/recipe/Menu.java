504400b4981edf62c1c2bfe936693f50c8643396
FrancescoGhinamo <44404163+FrancescoGhinamo@users.noreply.github.com>
2019-12-14 14:43:50 +0100

    Update Menu.java

---
+++ src/yumenu/backend/beam/recipe/Menu.java
@@ -0,0 +1,16 @@
+package yumenu.backend.beam.recipe;
+
+import java.util.ArrayList;
+
+public class Menu {
+
+	
+	private static final long serialVersionUID = -8133597377913764662L;
+	
+	private String name;
+	private ArrayList<Recipe> recipes;
+	
+	
+	
+
+}
