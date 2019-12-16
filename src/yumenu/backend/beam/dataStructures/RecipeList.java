dbc6cfce0720c98ab80a4d48177548a471add3d6
FrancescoGhinamo <44404163+FrancescoGhinamo@users.noreply.github.com>
2019-12-16 09:38:31 +0100

    Structure update

---
+++ src/yumenu/backend/beam/dataStructures/RecipeList.java
@@ -0,0 +1,12 @@
+package yumenu.backend.beam.dataStructures;
+
+import java.util.ArrayList;
+
+import yumenu.backend.beam.recipe.Recipe;
+
+public class RecipeList extends ArrayList<Recipe> {
+
+	
+	private static final long serialVersionUID = -6194629125092959054L;
+
+}
--- src/yumenu/backend/beam/recipe/Menu.java
+++ src/yumenu/backend/beam/recipe/Menu.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 public class Menu {
 
 	
-	private static final long serialVersionUID = -8133597377913764662L;
 	
 	private String name;
 	private ArrayList<Recipe> recipes;
